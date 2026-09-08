40ba54fc66d19bcb9a0bc722a35c0d2f9ec2f988
Mya P. Brown Founder, of The Commons <mommommy1960@gmail.com>
2026-09-08 04:55:02 -0700

    Export reference comparison engine

--- flux_drive_kernel/__init__.py
+++ flux_drive_kernel/__init__.py
@@ -4,7 +4,8 @@ from .bench import BenchConfig, BenchState, FluxDriveBench
 from .hil import HILReport, audit_csv, audit_rows, report_json
 from .relativity import hawking_lifetime_s, rest_energy_joules, schwarzschild_radius_m
 from .metrology import CalibratedMeasurement, calibrate_linear, is_consistent_with_zero, momentum_closure
+from .reference import ReferenceObservation, force_to_power_N_W, load_reference_csv, photon_pressure_force_N
 
-__all__ = ["BenchConfig", "BenchState", "FluxDriveBench", "HILReport", "audit_csv", "audit_rows", "report_json", "hawking_lifetime_s", "rest_energy_joules", "schwarzschild_radius_m", "CalibratedMeasurement", "calibrate_linear", "is_consistent_with_zero", "momentum_closure"]
+__all__ = ["BenchConfig", "BenchState", "FluxDriveBench", "HILReport", "audit_csv", "audit_rows", "report_json", "hawking_lifetime_s", "rest_energy_joules", "schwarzschild_radius_m", "CalibratedMeasurement", "calibrate_linear", "is_consistent_with_zero", "momentum_closure", "ReferenceObservation", "force_to_power_N_W", "load_reference_csv", "photon_pressure_force_N"]
 
 __version__ = "0.1.0"
