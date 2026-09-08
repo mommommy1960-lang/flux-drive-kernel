f4d11310bf29f7568d774304126240171be669c4
Mya P. Brown Founder, of The Commons <mommommy1960@gmail.com>
2026-09-08 03:57:28 -0700

    flux-drive-kernel: add safe actuator-bench baseline

---
+++ flux_drive_kernel/__init__.py
@@ -0,0 +1,8 @@
+"""Safe, measurement-first Flux Drive actuator-bench kernel."""
+
+from .bench import BenchConfig, BenchState, FluxDriveBench
+
+__all__ = ["BenchConfig", "BenchState", "FluxDriveBench"]
+
+__version__ = "0.1.0"
+
