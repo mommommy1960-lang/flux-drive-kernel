ffcc3e6e6c38c65cdb1ad5714390990cddc0c135
Mya P. Brown Founder, of The Commons <mommommy1960@gmail.com>
2026-09-08 04:47:52 -0700

    Export metrology engine

--- flux_drive_kernel/__init__.py
+++ flux_drive_kernel/__init__.py
@@ -3,7 +3,8 @@
 from .bench import BenchConfig, BenchState, FluxDriveBench
 from .hil import HILReport, audit_csv, audit_rows, report_json
 from .relativity import hawking_lifetime_s, rest_energy_joules, schwarzschild_radius_m
+from .metrology import CalibratedMeasurement, calibrate_linear, is_consistent_with_zero, momentum_closure
 
-__all__ = ["BenchConfig", "BenchState", "FluxDriveBench", "HILReport", "audit_csv", "audit_rows", "report_json", "hawking_lifetime_s", "rest_energy_joules", "schwarzschild_radius_m"]
+__all__ = ["BenchConfig", "BenchState", "FluxDriveBench", "HILReport", "audit_csv", "audit_rows", "report_json", "hawking_lifetime_s", "rest_energy_joules", "schwarzschild_radius_m", "CalibratedMeasurement", "calibrate_linear", "is_consistent_with_zero", "momentum_closure"]
 
 __version__ = "0.1.0"
