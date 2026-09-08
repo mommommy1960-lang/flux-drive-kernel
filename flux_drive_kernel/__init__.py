34455fce0d6a5b2a4f0db746be6f340cf6c717bd
Mya P. Brown Founder, of The Commons <mommommy1960@gmail.com>
2026-09-08 04:05:17 -0700

    Export HIL audit API

--- flux_drive_kernel/__init__.py
+++ flux_drive_kernel/__init__.py
@@ -1,8 +1,8 @@
 """Safe, measurement-first Flux Drive actuator-bench kernel."""
 
 from .bench import BenchConfig, BenchState, FluxDriveBench
+from .hil import HILReport, audit_csv, audit_rows, report_json
 
-__all__ = ["BenchConfig", "BenchState", "FluxDriveBench"]
+__all__ = ["BenchConfig", "BenchState", "FluxDriveBench", "HILReport", "audit_csv", "audit_rows", "report_json"]
 
 __version__ = "0.1.0"
-
