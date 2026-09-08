73ae314854f474c81fe59842d95b8b2e74dbe07a
Mya P. Brown Founder, of The Commons <mommommy1960@gmail.com>
2026-09-08 04:40:16 -0700

    Export relativity scale calculations

--- flux_drive_kernel/__init__.py
+++ flux_drive_kernel/__init__.py
@@ -2,7 +2,8 @@
 
 from .bench import BenchConfig, BenchState, FluxDriveBench
 from .hil import HILReport, audit_csv, audit_rows, report_json
+from .relativity import hawking_lifetime_s, rest_energy_joules, schwarzschild_radius_m
 
-__all__ = ["BenchConfig", "BenchState", "FluxDriveBench", "HILReport", "audit_csv", "audit_rows", "report_json"]
+__all__ = ["BenchConfig", "BenchState", "FluxDriveBench", "HILReport", "audit_csv", "audit_rows", "report_json", "hawking_lifetime_s", "rest_energy_joules", "schwarzschild_radius_m"]
 
 __version__ = "0.1.0"
